d315e6ebacbb6330ca148f80067833c3ce140daa
ice1000 <ice1000kotlin@foxmail.com>
2024-05-14 22:27:08 -0400

    simplify with `CharSequence`

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import manifold.ext.delegation.rt.api.link;
 
-public sealed interface I1 { void f1();}
-public record R() implements I1 { public void f1() {}}
-public record R2(@link R r) implements I1 { }
-
+public record Bruh(@link CharSequence cs) implements CharSequence { }
 public static void main(String[] args) {
   System.out.println("Hello world!");
 }
